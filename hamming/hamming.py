7fb03e09aaa3f2f1e22d5eb3d9a0727289682922
lkmandy <shafack.likhene@gmail.com>
2020-09-02 16:38:52 +0100

    Correct all test errors and optimize code

--- hamming/hamming.py
+++ hamming/hamming.py
@@ -1,10 +1,7 @@
 def distance(strand_a, strand_b):
-    i = count = 0
     if len(strand_a) != len(strand_b):
         raise ValueError("The Hamming distance cannot be measured for two DNA strands of different lengths")
     else:
-        while i < len(strand_a) - 1:
-            for a, b in strand_a, strand_a:
-                if a == b:
-                    count += 1
-    return count
+        compare = [a != b for a, b in zip(strand_a, strand_b) if a != b]
+        result = sum(compare)
+    return result
