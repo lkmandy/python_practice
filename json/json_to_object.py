1de365c07352c2d62e4ac676c509cf69e855de64
Amanda Shafack <shafack.likhene@gmail.com>
2021-12-26 23:47:53 +0100

    Call function to print json data properly

--- json/json_to_object.py
+++ json/json_to_object.py
@@ -3,12 +3,31 @@ import json
 
 def json_to_object():
     # sample json code snippet. Must be a byte, string or bytearray
-    json_url = '''{"employees":[  
-        {"name":"Shyam", "email":"shyamjaiswal@gmail.com"},  
-        {"name":"Bob", "email":"bob32@gmail.com"},  
-        {"name":"Jai", "email":"jai87@gmail.com"}  
-    ]}'''
+    json_url = '''{
+    "glossary": {
+        "title": "example glossary",
+		"GlossDiv": {
+            "title": "S",
+			"GlossList": {
+                "GlossEntry": {
+                    "ID": "SGML",
+					"SortAs": "SGML",
+					"GlossTerm": "Standard Generalized Markup Language",
+					"Acronym": "SGML",
+					"Abbrev": "ISO 8879:1986",
+					"GlossDef": {
+                        "para": "A meta-markup language, used to create markup languages such as DocBook.",
+						"GlossSeeAlso": ["GML", "XML"]
+                    },
+					"GlossSee": "markup"
+                }
+            }
+        }
+    }
+}'''
 
     # convert to object
     destination = json.loads(json_url)
     print(destination)
+
+json_to_object()
