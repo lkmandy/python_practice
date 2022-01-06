17d898ac9cfe9674b3c3bd31af9275b0b9fbfe06
Amanda Shafack <shafack.likhene@gmail.com>
2022-01-06 23:11:30 +0100

    Add custom functions to a Linked List

--- udacity_dsa_exercises/singly_linked_lists.py
+++ udacity_dsa_exercises/singly_linked_lists.py
@@ -9,8 +9,8 @@ Then, use "Test Run" and "Submit" to run the test cases
 at the bottom."""
 
 class Element(object):
-    def __init__(self, value):
-        self.value = value
+    def __init__(self, data):
+        self.data = data
         self.next = None
         
 class LinkedList(object):
@@ -30,6 +30,17 @@ class LinkedList(object):
         """Get an element from a particular position.
         Assume the first position is "1".
         Return "None" if position is not in the list."""
+        counter = 1
+        current_node = self.head
+
+        if position < 1:
+            return None
+        
+        while current_node and counter <= position:
+            if counter == position:
+                return current_node
+            current_node = current_node.next
+            counter += 1
         return None
     
     def insert(self, new_element, position):
@@ -37,12 +48,40 @@ class LinkedList(object):
         Assume the first position is "1".
         Inserting at position 3 means between
         the 2nd and 3rd elements."""
-        pass
-    
-    
+        counter = 1
+        current = self.head
+        if position > 1:
+            while current and counter < position:
+                if counter == position - 1:
+                    new_element.next = current.next
+                    current.next = new_element
+                current = current.next
+                counter += 1
+        # If we are inserting at the start of the linked list
+        elif position == 1:
+            new_element.next = self.head
+            self.head = new_element
+
     def delete(self, value):
-        """Delete the first node with a given value."""
-        pass
+
+        cur_node = self.head
+        prev = None 
+
+        # deleting the head of a linked list
+        if cur_node and cur_node.data == value:
+            self.head = cur_node.next
+            cur_node = None
+            return
+
+        while cur_node and cur_node.data != value:
+            prev = cur_node
+            cur_node = cur_node.next
+
+        if cur_node is None:
+            return 
+
+        prev.next = cur_node.next
+        cur_node = None
 
 # Test cases
 # Set up some Elements
@@ -56,22 +95,29 @@ ll = LinkedList(e1)
 ll.append(e2)
 ll.append(e3)
 
+print(ll.head.data)
+
 # Test get_position
 # Should print 3
-print ll.head.next.next.value
+print ll.head.next.next.data
+
 # Should also print 3
-print ll.get_position(3).value
+print ll.get_position(3).data
 
 # Test insert
 ll.insert(e4,3)
+
 # Should print 4 now
-print ll.get_position(3).value
+print ll.get_position(3).data
 
 # Test delete
 ll.delete(1)
+
 # Should print 2 now
-print ll.get_position(1).value
+print ll.get_position(1).data
+
 # Should print 4 now
-print ll.get_position(2).value
+print ll.get_position(2).data
+
 # Should print 3 now
-print ll.get_position(3).value
+print ll.get_position(3).data
