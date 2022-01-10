4b825f5908779c1035d18bdca8d8d582f03d64b6
Amanda Shafack <shafack.likhene@gmail.com>
2022-01-10 23:19:00 +0100

    Implement the fibonacci algorithm using recursion

---
+++ udacity_dsa_exercises/recursion_fibonacci.py
@@ -0,0 +1,27 @@
+from functools import lru_cache
+
+"""
+Implement a function recursively to get the desired Fibonacci sequence value.
+Your code should have the same input/output as the iterative code in the instructions.
+"""
+@lru_cache(max_size = 1000)
+def get_fib(position):
+    if position == 0:
+        return 0
+        
+    if position == 1 or position == 2:
+        return 1
+        
+    if position > 2:
+        return get_fib(position-1) + get_fib(position-2)
+        
+    if type(position) != int:
+        raise TypeError("The input must be an integer")
+    
+    if position < 1:
+        raise ValueError("The input must be an integer")
+    
+# Test cases
+print get_fib(9)
+print get_fib(11)
+print get_fib(1000)
